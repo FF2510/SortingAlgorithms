b1061b6cfcc6b2b57bbafbf1f0797d943d3efc2e
Frederic Faschina <faschina.frederic@icloud.com>
2024-08-09 04:48:26 +0200

    Interface:
    Commented Interface.

--- SortingAlgorithms/SortingAlgorithms/source/ISortingAlgorithm.cs
+++ SortingAlgorithms/SortingAlgorithms/source/ISortingAlgorithm.cs
@@ -1,7 +1,15 @@
 namespace SortingAlgorithms
 {
+    /// <summary>
+    /// Interface that defines a method to sort an array of integers.
+    /// Implementing classes will provide specific sorting algorithms.
+    /// </summary>
     public interface ISortingAlgorithm
     {
+        /// <summary>
+        /// Sorts an array of integers in ascending order.
+        /// </summary>
+        /// <param name="array">The array of integers to be sorted.</param>
         void Sort(int[] array);
     }
 }
